21faf935353979488706a99043b56ea14839fd7a
Krystan K <Korn0018@algonquinlive.com>
2025-11-22 02:29:45 -0500

    Added the Worker routine with fetching URLs

--- main.go
+++ main.go
@@ -2,41 +2,76 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"strconv"
 )
 
-//"sync"
-//"time"
+// to run, simply say in the terminal "go run practiceTasks.go" in the terminal
+// code written in VS code
+type FetchResult struct {
+	URL        string
+	StatusCode int
+	Size       int
+	Error      error
+}
 
-//to run, simply say in the terminal "go run practiceTasks.go" in the terminal
-//code written in VS code
-
-func worker(id int, jobs <-chan int, results chan<- int) {
-	for j := range jobs {
-		fmt.Printf("Worker %d processing job %d\n", id, j)
-		time.Sleep(500 * time.Millisecond)
-		results <- j * 2
+func worker(id int, jobs <-chan string, results chan<- FetchResult) {
+	//defer wg.Done()
+	//TODO: fetch the URL
+	//To read the URL from the terminal, you can do this
+	//Reference used: https://www.geeksforgeeks.org/go-language/golang-program-to-check-status-of-a-url-website/
+	// var url string
+	// fmt.Print("Enter the URL of the website: ")
+	// fmt.Scan(&url)
+	for j := 1; j <= len(jobs); j++ {
+		if j == id {
+			var url string
+			fmt.Print("Enter the URL of the website: ")
+			fmt.Scan(&url)
+			//use jobs?
+			resp, err := http.Get(url)
+			//TODO: send Result struct to results channel
+			if err != nil {
+				results <- FetchResult{URL: url, Error: err}
+			}
+			defer resp.Body.Close()
+		}
 	}
+	//results <- res
+	//hint: use resp, err := http.Get(url)
 }
 func main() {
-	//4th task
-	jobs := make(chan int, 10)
-	results := make(chan int, 10)
+	//var wg sync.WaitGroup
+	urls := []string{
+		"https://example.com",
+		"https://golang.org",
+		"https://uottawa.ca",
+		"https://github.com",
+		"https://httpbin.org/get",
+	}
 
-	//Start 3 workers
-	for w := 1; w <= 3; w++ {
+	numWorkers := 5
+	numJobs := 5
+
+	jobs := make(chan string, len(urls))
+	results := make(chan FetchResult, len(urls))
+
+	//TODO: Start workers
+	for w := 1; w <= numWorkers; w++ {
+		//wg.Add(1)
 		go worker(w, jobs, results)
 	}
 
-	//Send 5 jobs
-	for j := 1; j <= 5; j++ {
-		jobs <- j
+	//TODO: Send jobs
+	for j := 1; j <= numJobs; j++ {
+		jobs <- strconv.Itoa(j)
 	}
 	close(jobs)
 
-	//Collect results
-	for i := 1; i <= 5; i++ {
+	//TODO: Collect results
+	for i := 1; i <= numJobs; i++ {
 		fmt.Println("Results:", <-results)
 	}
 
+	fmt.Println("\nScraping complete!")
 }
